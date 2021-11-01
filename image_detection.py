0181a71f7a29335bc4923f545d42b163ca106f4c
stevekola <kolawolesteven99@gmail.com>
2021-11-01 03:05:16 +0100

    add deployment scripts

---
+++ image_detection.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:eecbaaa7f42d1e832a9893b0cd4287c97835a6a7a55cef4c150b86a0bdbb2f6a
+size 1747
---
+++ livefeed_detection.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0e5555528462f0672333975da200d2f051db1434d47830166afc46e03f4db5d8
+size 2688
